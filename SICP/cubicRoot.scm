02941df3c806183274c5f7717de2c9a3c6a9e1d8
wellpardim <wellingtons_10@hotmail.com>
2016-01-12 21:33:19 -0200

    ident

--- SICP/cubicRoot.scm
+++ SICP/cubicRoot.scm
@@ -1,21 +1,21 @@
  
- (define (square x) (* x x)) 
-  
- (define (cube-root-iter guess prev-guess x) 
-   (if (good-enough? guess prev-guess) 
-       guess 
-       (cube-root-iter (improve guess x) guess x))) 
-  
- (define (improve guess x) 
-   (average3 (/ x (square guess))  guess guess)) 
-  
- (define (average3 x y z ) 
-   (/ (+ x y z) 3)) 
-  
- (define (good-enough? guess prev-guess) 
-   (< (abs (- guess prev-guess)) (abs (* guess 0.0001)))) 
-  
- (define (cube-root x) 
-   (cube-root-iter 1.0 0.0 x)) 
-  (define (cube-root x) 
- ((if (< x 0) - +)(cube-root-iter (improve 1.0 (abs x)) 1 (abs x)))) 
+(define (square x) (* x x)) 
+
+(define (cube-root-iter guess prev-guess x) 
+	(if (good-enough? guess prev-guess) 
+		guess 
+		(cube-root-iter (improve guess x) guess x))) 
+
+(define (improve guess x) 
+	(average3 (/ x (square guess))  guess guess)) 
+
+(define (average3 x y z ) 
+	(/ (+ x y z) 3)) 
+
+(define (good-enough? guess prev-guess) 
+	(< (abs (- guess prev-guess)) (abs (* guess 0.0001)))) 
+
+(define (cube-root x) 
+	(cube-root-iter 1.0 0.0 x)) 
+(define (cube-root x) 
+	((if (< x 0) - +)(cube-root-iter (improve 1.0 (abs x)) 1 (abs x)))) 
